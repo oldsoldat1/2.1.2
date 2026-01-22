ea36f050ac74bad96bb027716acc4a26ae4ef157
Волох Алексей Николаевич <Voloh.a@dns-shop.ru>
2026-01-22 15:01:39 +0600

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean1 == bean2);
+        System.out.println(cat1 == cat2);
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,18 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat cat(){
+
+        Cat cat = new Cat();
+        cat.setName("Barsik");
+        return cat;
+
+
+    }
+
+
+
 }
+
